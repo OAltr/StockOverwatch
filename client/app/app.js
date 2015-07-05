e1676a7e2eeee6954f05cfb8b528daac7e6c85c2
Oscar <oscar.altrogge@me.com>
2015-07-06 00:36:05 +0200

    Added Chart.js

--- client/app/app.js
+++ client/app/app.js
@@ -1,16 +1,17 @@
 'use strict';
 
 angular.module('stockOverwatchApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'btford.socket-io',
-  'ui.router',
-  'ui.bootstrap'
+	'ngCookies',
+	'ngResource',
+	'ngSanitize',
+	'btford.socket-io',
+	'ui.router',
+	'ui.bootstrap',
+	'chart.js'
 ])
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
-    $urlRouterProvider
-      .otherwise('/');
+	.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+		$urlRouterProvider
+			.otherwise('/');
 
-    $locationProvider.html5Mode(true);
-  });
+		$locationProvider.html5Mode(true);
+	});
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,6 +3,18 @@
 angular.module('stockOverwatchApp')
 	.controller('MainCtrl', function ($scope, $http, socket) {
 		$scope.theStocks = [];
+		$scope.series = ['AAPL', 'FB'];
+		$scope.data = [
+			[1,3,2,7,3,1],
+			[3,2,7,3,1,1]
+		];
+		$scope.labels = ['One', 'Two', 'Three', 'Four', 'Five', 'Six'];
+		$scope.options = {
+			responsive: true,
+			scaleShowGridLines : false,
+			bezierCurve : false,
+			pointDot : false
+		};
 
 		$http.get('/api/stocks').success(function(theStocks) {
 			$scope.theStocks = theStocks;
