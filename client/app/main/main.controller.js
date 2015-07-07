060fe0f02657ea477a7883f3f5fc8a24be192c11
Oscar <oscar.altrogge@me.com>
2015-07-07 21:17:04 +0200

    Fixed a bug with new stock An empty input caused an error because newStock was undefined

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,13 +3,14 @@
 angular.module('stockOverwatchApp')
 	.controller('MainCtrl', function ($scope, $http, socket) {
 		Date.prototype.yyyymmdd = function() {
-		  var yyyy = this.getFullYear().toString();
-		  var mm = (this.getMonth()+1).toString();
-		  var dd  = this.getDate().toString();
-		  return yyyy + '-' + (mm[1]?mm:'0'+mm[0]) + '-' + (dd[1]?dd:'0'+dd[0]);
+			var yyyy = this.getFullYear().toString();
+			var mm = (this.getMonth()+1).toString();
+			var dd  = this.getDate().toString();
+			return yyyy + '-' + (mm[1]?mm:'0'+mm[0]) + '-' + (dd[1]?dd:'0'+dd[0]);
 		};
 
 		$scope.theStocks = [];
+		$scope.newStock = '';
 		$scope.series = [];
 		$scope.data = [];
 		$scope.labels = [];
